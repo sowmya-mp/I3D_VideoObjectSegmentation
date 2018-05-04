1acc56007c20440eee0d8711832598887cdf8513
sowmya-mp <sowmya.mp99@gmail.com>
2018-05-04 18:29:14 -0400

    Thresholding random crops

--- dataloaders/custom_transforms.py
+++ dataloaders/custom_transforms.py
@@ -3,8 +3,9 @@ import cv2
 import numpy as np
 import torch
 import scipy.misc as spy
-import torchvision.transforms 
+import torchvision.transforms
 import PIL
+
 
 class ScaleNRotate(object):
     """Scale (zoom-in, zoom-out) and Rotate the image and the ground truth.
@@ -15,6 +16,7 @@ class ScaleNRotate(object):
         2.  rots [list]: list of fixed possible rotation angles
             scales [list]: list of fixed possible scales
     """
+
     def __init__(self, rots=(-30, 30), scales=(.75, 1.25)):
         assert (isinstance(rots, type(scales)))
         self.rots = rots
@@ -25,7 +27,7 @@ class ScaleNRotate(object):
         if type(self.rots) == tuple:
             # Continuous range of scales and rotations
             rot = (self.rots[1] - self.rots[0]) * random.random() - \
-                  (self.rots[1] - self.rots[0])/2
+                  (self.rots[1] - self.rots[0]) / 2
 
             sc = (self.scales[1] - self.scales[0]) * random.random() - \
                  (self.scales[1] - self.scales[0]) / 2 + 1
@@ -42,7 +44,7 @@ class ScaleNRotate(object):
 
             h, w = tmp.shape[:2]
             center = (w / 2, h / 2)
-            assert(center != 0)  # Strange behaviour warpAffine
+            assert (center != 0)  # Strange behaviour warpAffine
             M = cv2.getRotationMatrix2D(center, rot, sc)
 
             if ((tmp == 0) | (tmp == 1)).all():
@@ -61,6 +63,7 @@ class Resize(object):
     Args:
         scales (list): the list of scales
     """
+
     def __init__(self, scales=[0.5, 0.8, 1]):
         self.scales = scales
 
@@ -91,6 +94,7 @@ class VideoResize(object):
     Args:
         size (list): the list of sizes
     """
+
     def __init__(self, sizes=[224, 224]):
         self.sizes = sizes
 
@@ -101,7 +105,6 @@ class VideoResize(object):
                 continue
             tmp = sample[elem]
 
-
             flagval = 'bilinear'
 
             num_frames = tmp.shape[0]
@@ -111,13 +114,13 @@ class VideoResize(object):
             for frameIndex in range(num_frames):
                 if isGT:
                     toAppend = spy.imresize(tmp[frameIndex, :, :, 0], (self.sizes[0], self.sizes[1]), interp=flagval)
-                    res.append(np.reshape(toAppend,(self.sizes[0], self.sizes[1],1)))
+                    res.append(np.reshape(toAppend, (self.sizes[0], self.sizes[1], 1)))
                 else:
                     res.append(spy.imresize(tmp[frameIndex, :, :, :], (self.sizes[0], self.sizes[1]), interp=flagval))
-            tmp = np.array(res,dtype="float32")
+            tmp = np.array(res, dtype="float32")
             tmp = np.transpose(tmp, (0, 3, 1, 2))
 
-            #siddhant: We are normalizing here because i3d expects normalized image values
+            # siddhant: We are normalizing here because i3d expects normalized image values
             # For Ground truth the imresize function converts 0-1 values to 0-255, so we are converting it back
             tmp = np.array(tmp, dtype=np.float32)
             tmp = tmp / np.max([tmp.max(), 1e-8])
@@ -125,11 +128,13 @@ class VideoResize(object):
 
         return sample
 
+
 class VideoLucidDream(object):
     """Resizes the set of videos frames and the ground truths to specified pixel values.
     Args:
         size (list): the list of sizes
     """
+
     def __init__(self, sizes=[224, 224]):
         self.sizes = sizes
 
@@ -147,62 +152,82 @@ class VideoLucidDream(object):
             if random.random() < 0.5:
                 isRotate[i] = False
         seed = 12345
-        for elem in sample.keys():
-            if 'fname' in elem:
-                continue
-            tmp = sample[elem]
 
+        input_tmp = sample['image']
+        gt_tmp = sample['gt']
 
-            flagval = 'bilinear'
+        flagval = 'bilinear'
 
-            num_frames = tmp.shape[0]
-            tmp = np.transpose(tmp, (0, 2, 3, 1))
-            res = []
-            isGT = tmp.shape[3] == 1
-            for frameIndex in range(num_frames):
-                random.seed(frameIndex)
-                randCropTransform = torchvision.transforms.RandomCrop(self.sizes, padding=0)
-                if isGT:
-                    imageToCrop = PIL.Image.fromarray(tmp[frameIndex, :, :, 0])
-                    toAppend = randCropTransform(imageToCrop)
-                    toAppend = np.array(toAppend)
-                    toAppend = np.reshape(toAppend,(self.sizes[0], self.sizes[1],1))
-                    if isFlip[frameIndex]:
-                        toAppend = cv2.flip(toAppend, flipCode=1)
-                    #if isRotate[frameIndex]:
-                    #    M = cv2.getRotationMatrix2D((toAppend.shape[0]/2,toAppend.shape[1]/2),90,1)
-                    #    toAppend = cv2.warpAffine(toAppend,M,(toAppend.shape[0],toAppend.shape[1]))
-
-                    res.append(np.reshape(toAppend,(self.sizes[0], self.sizes[1],1)))
+        num_frames = input_tmp.shape[0]
+        input_tmp = np.transpose(input_tmp, (0, 2, 3, 1))
+        gt_tmp = np.transpose(gt_tmp, (0, 2, 3, 1))
+        height = input_tmp.shape[2]
+        width = input_tmp.shape[1]
+        res_input = []
+        res_gt = []
+        for frameIndex in range(num_frames):
+            # random.seed(frameIndex)
+            #randCropTransform = torchvision.transforms.RandomCrop(self.sizes, padding=0)
+            left = np.random.randint(0, width-1-224)
+            top = np.random.randint(0, height-1-224)
+            while True:
+                imageToCrop = gt_tmp[frameIndex, :, :, 0]
+                cropped_gt = imageToCrop[left:left+224,top:top+224]
+                threshold = np.sum(cropped_gt) / float(height*width)
+                if threshold > 0.002:
+                    break
                 else:
-                    imageToCrop = PIL.Image.fromarray(np.asarray(tmp[frameIndex, :, :, :],dtype=np.uint8),'RGB')
-                    toAppend = randCropTransform(imageToCrop)
-                    toAppend = np.array(toAppend)
-                    if isFlip[frameIndex]:
-                        toAppend = cv2.flip(toAppend, flipCode=1)
-                    #if isRotate[frameIndex]:
-                    #    M = cv2.getRotationMatrix2D((toAppend.shape[0]/2,toAppend.shape[1]/2),90,1)
-                    #    toAppend = cv2.warpAffine(toAppend,M,(toAppend.shape[0],toAppend.shape[1]))
-                    if isJitter[frameIndex]:
-                        noise = np.random.randint(0, 5, (toAppend.shape[0], toAppend.shape[1]))  # design jitter/noise here
-                        zitter = np.zeros_like(toAppend)
-                        zitter[:, :, 1] = noise
+                    left = np.random.randint(0, width - 1 - 224)
+                    top = np.random.randint(0, height - 1 - 224)
 
-                        toAppend = cv2.add(toAppend, zitter)
+            # GT transformations
+            imageToCrop = gt_tmp[frameIndex, :, :, 0]
+            toAppend = imageToCrop[left:left+224, top:top+224]
+            toAppend = np.reshape(toAppend, (self.sizes[0], self.sizes[1], 1))
+            if isFlip[frameIndex]:
+                toAppend = cv2.flip(toAppend, flipCode=1)
+            # if isRotate[frameIndex]:
+            #    M = cv2.getRotationMatrix2D((toAppend.shape[0]/2,toAppend.shape[1]/2),90,1)
+            #    toAppend = cv2.warpAffine(toAppend,M,(toAppend.shape[0],toAppend.shape[1]))
 
-                    res.append(toAppend)
+            res_gt.append(np.reshape(toAppend, (self.sizes[0], self.sizes[1], 1)))
 
-            tmp = np.array(res,dtype="float32")
-            tmp = np.transpose(tmp, (0, 3, 1, 2))
+            #Input transformations
+            imageToCrop = input_tmp[frameIndex, :, :, :]
+            toAppend = imageToCrop[left:left+224, top:top+224]
+            if isFlip[frameIndex]:
+                toAppend = cv2.flip(toAppend, flipCode=1)
+            # if isRotate[frameIndex]:
+            #    M = cv2.getRotationMatrix2D((toAppend.shape[0]/2,toAppend.shape[1]/2),90,1)
+            #    toAppend = cv2.warpAffine(toAppend,M,(toAppend.shape[0],toAppend.shape[1]))
+            if isJitter[frameIndex]:
+                noise = np.random.randint(0, 5, (toAppend.shape[0], toAppend.shape[1]))  # design jitter/noise here
+                zitter = np.zeros_like(toAppend)
+                zitter[:, :, 1] = noise
 
-            #siddhant: We are normalizing here because i3d expects normalized image values
-            # For Ground truth the imresize function converts 0-1 values to 0-255, so we are converting it back
-            tmp = np.array(tmp, dtype=np.float32)
-            tmp = tmp / np.max([tmp.max(), 1e-8])
-            sample[elem] = tmp
+                toAppend = cv2.add(toAppend, zitter)
+
+            res_input.append(toAppend)
+
+        input_tmp = np.array(res_input, dtype="float32")
+        input_tmp = np.transpose(input_tmp, (0, 3, 1, 2))
+
+        # siddhant: We are normalizing here because i3d expects normalized image values
+        # For Ground truth the imresize function converts 0-1 values to 0-255, so we are converting it back
+        input_tmp = np.array(input_tmp, dtype=np.float32)
+        input_tmp = input_tmp / np.max([input_tmp.max(), 1e-8])
+        sample['image'] = input_tmp
+
+        gt_tmp = np.array(res_gt, dtype="float32")
+        gt_tmp = np.transpose(gt_tmp, (0, 3, 1, 2))
+
+        # siddhant: We are normalizing here because i3d expects normalized image values
+        # For Ground truth the imresize function converts 0-1 values to 0-255, so we are converting it back
+        gt_tmp = np.array(gt_tmp, dtype=np.float32)
+        gt_tmp = gt_tmp / np.max([gt_tmp.max(), 1e-8])
+        sample['gt'] = gt_tmp
 
         return sample
-
 
 
 class RandomHorizontalFlip(object):
@@ -234,11 +259,11 @@ class ToTensor(object):
             if tmp.ndim == 2:
                 tmp = tmp[:, :, np.newaxis]
 
-            # swap color axis because
-            # numpy image: H x W x C
-            # torch image: C X H X W
+                # swap color axis because
+                # numpy image: H x W x C
+                # torch image: C X H X W
 
-#            tmp = tmp.transpose((2, 0, 1))
+            #            tmp = tmp.transpose((2, 0, 1))
             sample[elem] = torch.from_numpy(tmp)
 
         return sample
